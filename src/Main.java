05d5a249bba978ef9769856fffe1012bfa465e76
RyanRafaelA <ryrafael461@gmail.com>
2025-01-31 21:23:18 -0300

    Base do main concluida

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        int num1;
+        int num2;
+
+        System.out.print("Digite o 1ª numero: ");
+        num1 = sc.nextInt();
+        System.out.print("Digite o 2ª numero: ");
+        num2 = sc.nextInt();
+
+        try{
+            contar(num1, num2);
+        }catch (ParametrosInvalidosException e){
+            System.out.println("Exceção: "+e.getMessage());
+        }
     }
 }
